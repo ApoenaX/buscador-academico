20b76150c9ca3c5564eead331134b6026b1c1e29
Jessica <jessicasousa.pc@gmail.com>
2022-01-16 16:29:58 -0300

    :sparkles: feat: added subset search

--- src/SearchContainer.js
+++ src/SearchContainer.js
@@ -10,9 +10,14 @@ export const SearchContainer = ({
 	setError,
 }) => {
 	const [value, setValue] = useState('')
+	const [subsetValue, setSubsetValue] = useState('dissertacao')
 
 	const handleChange = (event) => {
 		setValue(event.target.value)
+	}
+
+	const handleSubsetChange = (event) => {
+		setSubsetValue(event.target.value)
 	}
 
 	const onSubmit = async (event) => {
@@ -21,7 +26,7 @@ export const SearchContainer = ({
 			setValue('')
 			setDisplayWelcome(false)
 			setIsLoading(true)
-			await fetch(`${API_URL}/search/?q=${value}`)
+			await fetch(`${API_URL}/search/?q=${value}&key=${subsetValue}`)
 				.then((res) => res.json())
 				.then((json) => {
 					setError(null)
@@ -49,6 +54,46 @@ export const SearchContainer = ({
 				<button>
 					<i className='fa fa-search icon'></i>
 				</button>
+				<label htmlFor='subset'>Escolha uma categoria: </label>
+				<select
+					name='subset'
+					id='subset'
+					onChange={handleSubsetChange}
+					value={subsetValue}
+				>
+					<option value='dissertacao'>Teses e Dissertações</option>
+					<option value='financiadores'>Financiadores</option>
+					<option value='musica'>Música</option>
+					<option value='arte_cenica'>Artes cênicas</option>
+					<option value='arte_visual'>Artes visuais</option>
+					<option value='trab_anais'>Trabalho em anais</option>
+					<option value='artigo_revista'>Artigo em revista</option>
+					<option value='livro'>Livro</option>
+					<option value='partitura'>Partitura</option>
+					<option value='traducao'>Tradução</option>
+					<option value='apresentacao_trab'>
+						Apresentacao de trabalho
+					</option>
+					<option value='cartas_mapas'>cartas_mapas</option>
+					<option value='curso_curto'>curso_curto</option>
+					<option value='aplicativo'>Aplicativo</option>
+					<option value='tecnica'>Técnica</option>
+					<option value='mat_didatico'>Matérial didático</option>
+					<option value='editoria'>Editorial</option>
+					<option value='maquete'>Maquete</option>
+					<option value='manutencao_obra_art'>
+						Manutenção de obra artes
+					</option>
+					<option value='org_evento'>Organização de evento</option>
+					<option value='patente'>Patente</option>
+					<option value='prog_radio_tv'>
+						Progama de rádio e tv
+					</option>
+					<option value='relatorio_pesq'>
+						Relatório de pesquisa
+					</option>
+					<option value='serv_tecnicos'>Serviços técnicos</option>
+				</select>
 			</form>
 		</header>
 	)
